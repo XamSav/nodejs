59a50ea9b824de3d861a69c5bb1d599ed765b8e4
Kiznaiver27 <a19samvigpul@inspedralbes.cat>
2020-12-03 22:56:48 +0100

    .

--- 2damvi-server-1/src/server.js
+++ 2damvi-server-1/src/server.js
@@ -11,31 +11,16 @@ const server = app.listen(port, () =>
     console.log("El servidor está inicializado en el puerto " + port
 ));
 
-
-
-
-
-
-
 //Parte de Websocket
 const SocketIo = require('socket.io');
 const { searcher } = require('./api.js');
 const io = SocketIo(server);
-
-
-
-
 
 //Escuchar evento de conexion
 io.on('connection', (socket) =>{
   console.log('Nueva conexion de', socket.id);
   console.log('Nueva conexion de', socket.handshake.address);
   
-
-
-
-
-
   //Escuchar evento
     //Crear un jugador
   socket.on('player:create',(data)=>{
@@ -44,12 +29,7 @@ io.on('connection', (socket) =>{
     if(ok === true){
     io.sockets.emit('server:playercreated', data)
     }
-    
   });
-
-
-
-
 
     //Actualizar un jugador
   socket.on('player:playerupdate',(data)=>{
@@ -57,18 +37,11 @@ io.on('connection', (socket) =>{
     io.sockets.emit('server:playerupdate', data)
   });
 
-
-
-  
     //Compra de Coins
   socket.on('player:buycoin',(data)=>{
     //Emitir a todos los usuarios
     io.sockets.emit('server:buyshop', data)
   });
-
-
-
-
 
     /*
   socket.on('player:onlyadata', (data) =>{
